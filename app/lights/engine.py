873105666cb9623dfff334aabc60172f49de4489
Nick Kortendick <nick@nickkortendick.com>
2016-03-05 13:28:45 -0600

    Add lights library

---
+++ app/lights/engine.py
@@ -0,0 +1,45 @@
+import requests
+import datetime
+from constants import *
+from secrets import access_token, device
+from time import sleep
+domain = "https://api.particle.io/v1/devices/{0}/{1}"
+
+def main():
+  i = 0
+  while True:
+    light_strip([orange, black, blue])
+    i = i + 1
+
+def illinois_forwards():
+  return light_strip_forwards(illinois, 30)
+
+def illinois_backwards():
+  return light_strip_backwards(illinois, 30)
+
+def light_strip(colors):
+  return send_request(colors, "color")
+
+def clear_strip():
+  return send_request([black], "color")
+
+def light_strip_forwards(colors, delay):
+    return send_request([str(delay)] + colors, "forwards")  
+
+def light_strip_backwards(colors, delay):
+    return send_request([str(delay)] + colors, "backwards")
+
+def send_request(colors, endpoint):
+  payload = {'access_token' : access_token, 'args' : ",".join(colors)}
+  response = requests.post(get_url(endpoint), data = payload)
+  print str(datetime.datetime.now()) + "\t : \t" + str(payload)
+  print str(datetime.datetime.now()) + "\t : \t" + response.text
+
+
+def get_url(endpoint):
+  if endpoint not in ['color', 'forwards', 'backwards']:
+    raise NameError(str(endpoint) + " is not a valid endpoint")
+  return domain.format(device, endpoint);
+
+if __name__ == '__main__':
+  main()
